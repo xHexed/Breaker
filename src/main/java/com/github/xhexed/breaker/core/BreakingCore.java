1fc2afd851c58a07488fc04a81e38f52a1169e14
xHexed <minhquanpct7b@gmail.com>
2020-06-01 20:27:53 +0800

    Only deny air "breaking"

--- src/main/java/com/github/xhexed/breaker/core/BreakingCore.java
+++ src/main/java/com/github/xhexed/breaker/core/BreakingCore.java
@@ -16,16 +16,13 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
-import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 
 import static com.github.xhexed.breaker.Breaker.getPlugin;
 
 public class BreakingCore {
     final Map<Integer, HashMap<String, BreakingBlock>> cachedBlocks = new HashMap<>();
-    private static final Set<Material> excludedMaterials = EnumSet.of(Material.AIR, Material.GRASS, Material.END_ROD, Material.BARRIER, Material.TORCH, Material.REDSTONE_TORCH_ON, Material.REDSTONE_TORCH_OFF, Material.LONG_GRASS, Material.BEETROOT_BLOCK, Material.WHEAT, Material.POTATO, Material.CARROT, Material.SAPLING, Material.FLOWER_POT, Material.YELLOW_FLOWER, Material.RED_ROSE, Material.DOUBLE_PLANT, Material.WATER_LILY, Material.FIRE, Material.DEAD_BUSH, Material.MELON_STEM, Material.PUMPKIN_STEM, Material.BROWN_MUSHROOM, Material.RED_MUSHROOM, Material.NETHER_WART_BLOCK, Material.REDSTONE_WIRE, Material.REDSTONE_COMPARATOR_OFF, Material.REDSTONE_COMPARATOR_ON, Material.SLIME_BLOCK, Material.DIODE_BLOCK_OFF, Material.DIODE_BLOCK_ON, Material.STRUCTURE_VOID, Material.SUGAR_CANE_BLOCK, Material.TNT, Material.TRIPWIRE, Material.TRIPWIRE_HOOK);
 
     @SuppressWarnings("deprecation")
     public BreakingCore() {
@@ -39,7 +36,7 @@ public class BreakingCore {
                 final BlockPosition bp = packet.getBlockPositionModifier().read(0);
                 final Block block = player.getWorld().getBlockAt(bp.getX(), bp.getY(), bp.getZ());
                 if (block == null ||
-                        excludedMaterials.contains(block.getType()) ||
+                        block.getType() == Material.AIR ||
                         !Breaker.getPlugin().database.has(block.getType(), block.getData())
                 ) return;
                 final BreakingBlock breakingBlock;
